932ee2a34c987d3ca28a79f53238d111f82a480a
harshit <harshit@harshit.com>
2021-08-31 18:35:39 +0530

    fixed backend

--- Express/app.js
+++ Express/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 80;
+const router = express.Router();
 
 app.use('/static', express.static('static'));
 
@@ -11,7 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res)=>{
     //const params = {'title': 'whatsapp clone'};
-    res.sendFile('/Project/WhatsappClone/Express/views/main.html');
+    res.sendFile(path.join(__dirname+'/views/main.html'));
 })
 
 app.listen(port, ()=>{
