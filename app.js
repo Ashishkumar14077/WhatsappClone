729b033c677114075f687d0d9b12e861f2d8eb30
Ashish <pintubrajkishor@gmail.com>
2021-09-12 20:06:41 +0530

    merged SIGNUP to main

--- app.js
+++ app.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-const express = require("express");
-const path = require("path");
-const app = express();
-const port = 80;
-const router = express.Router();
-
-app.use('/static', express.static('static'));
-
-//app.set('view-engine', 'html');
-
-app.set('views', path.join(__dirname, 'views'));
-
-app.get('/', (req, res)=>{
-    //const params = {'title': 'whatsapp clone'};
-    res.sendFile(path.join(__dirname+'/views/main.html'));
-})
-
-app.listen(port, ()=>{
-    console.log(`the application is running at http://localhost:${port}`);
-=======
 require("dotenv").config()
 const express = require("express");
 const passport = require("passport");
@@ -133,5 +112,4 @@ app.post("/loginDB",async(req, res)=>{
 })
 app.listen(port, ()=>{
     console.log(`the application is running at port http://localhost:${port}`);
->>>>>>> SIGNUP
 });
